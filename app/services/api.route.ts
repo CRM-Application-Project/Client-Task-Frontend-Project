63304b77cb0d2afe3e61e6d66c7665c7b837b73a
jayanthm45 <jayanth.m@seabed2crest.com>
2025-08-15 19:24:26 +0530

    added changes

--- app/services/api.route.ts
+++ app/services/api.route.ts
@@ -5,5 +5,13 @@ REGISTER:"/register",
   LOGIN:"/login",
 
   },
+  TASK:{
+    CREATE: "/tasks/",
+    UPDATE: "/tasks/{taskId}",
+    DELETE: "/tasks/{taskId}",
+    GET_BY_ID: "/tasks/{taskId}",
+      FILTER: "/tasks/filter",
+
+  }
 
 }
